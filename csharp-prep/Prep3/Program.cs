178c6e21f384d93a72ffbe489df80c8b597f2c46
lfglfernando <lfglfernandog@gmail.com>
2024-04-27 23:11:46 -0600

    Guess number prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int attempts = 0; 
+
+        Console.WriteLine("Guees a number between 1 and 100!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine()); 
+            attempts++; 
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! It took you {attempts} attempts.");
+            }
+        }
     }
 }
