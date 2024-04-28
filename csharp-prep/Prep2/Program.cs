0d00a490ace6172b4d5071d4c936dbe1fee0eacd
lfglfernando <lfglfernandog@gmail.com>
2024-04-27 22:56:47 -0600

    Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is Prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string gradePercentage = Console.ReadLine();
+
+        int note = int.Parse(gradePercentage);
+        string letter;
+
+        if (note >= 90)
+        {
+            letter = "A";
+        }
+        else if (note >= 80)
+        {
+            letter = "B";
+        }
+        else if (note >= 70)
+        {
+            letter = "C";
+        }
+        else if (note >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is a {letter}.");
+
+        // Check if the course is passed or failed
+        if (note >= 70)
+        {
+            Console.WriteLine("Great job! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course, try harder next time.");
+        }
     }
 }
